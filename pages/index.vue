f24c7a8c8c3b6382818e0da2b707b0a31d05561c
Dr. Lau Cher Han <laucherhan@gmail.com>
2022-03-25 09:45:19 +0800

    updated template

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Build your dream web application.</p>
+      <p class="mt-5">Turn your ideas into working web applications.</p>
     </div>
   </div>
 </template>
