c1e4904816c93f2ddf51a15bcd763607fc0913ce
WiseLee <wiseleelee@gmail.com>
2022-03-30 20:37:58 +0800

    updated homepage with new title and slogan.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div class="w-screen h-screen bg-blue-200 p-20">
+    <div class="text-black-700 text-2xl">
+      <img src="https://www.thelead.io/wp-content/uploads/2018/06/lead-logo-.png" />
+      <h1 class="text-5xl">Welcome to LEAD Pharmacy!</h1>
+      <p class="mt-5"><em>Your Healthcare Partner for Life.</em></p>
     </div>
+    <div class="flex w-full bg-blue-500 md:bg-green-200 p-14">
+      <h2 class="text-3xl">Please let us know your healthcare needs!</h2>
+      </div>
   </div>
 </template>
