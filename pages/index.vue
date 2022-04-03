bf8dad1c76dcf85d52a4838a1df4edc62569ccb2
WiseLee <wiseleelee@gmail.com>
2022-04-03 19:49:53 +0800

    template for burger shop

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="w-screen h-screen bg-blue-200 p-20">
-    <div class="text-black-700 text-2xl">
-      <img src="https://www.thelead.io/wp-content/uploads/2018/06/lead-logo-.png" />
-      <h1 class="text-5xl">Welcome to LEAD Pharmacy!</h1>
-      <p class="mt-5"><em>Your Healthcare Partner for Life.</em></p>
-    </div>
-    <div class="flex w-full bg-blue-500 md:bg-green-200 p-14">
-      <h2 class="text-3xl">Please let us know your healthcare needs!</h2>
-      </div>
+<div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+  <div id="menu" class="grid grid-cols-2 py-5">
+  <div id="menu-left">
+    <img src="logo.png" class="w-[150px]" alt="" />
   </div>
+
+    <div id="menu-right">
+      <ul class="grid grid-cols-4 text-lg font-oswa text-white uppercase pt-5 text-overflow: ellipsis">
+        <li>About Us</li>
+        <li>Our Menu</li>
+        <li>Locations</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+    </div>
+
+<div class="grid grid-cols-2">
+  <img src="hero-burger.png" class="pr-8 mt-10" alt="" />
+  <div>
+    <h1 class="text-8xl text-white text-center font-lilita uppercase">
+      Classic Burger
+      </h1>
+    <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">With Onion</h2>
+    <p class="text-white font-roboto font light">
+      Our Signature burger now comes with onion and cheese. You can customize it to your likings. Freshly made to order, all in a Brioche style bun.
+    </p>
+  </div>
+</div>
+</div>
 </template>
