c7b51d2a76611606ef2f40900f269f4b64265280
WiseLee <wiseleelee@gmail.com>
2022-05-03 22:48:49 +0800

    day8 exercise

---
+++ pages/menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="bg-[url('/menu-page.jpg)]">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+
+        <div id="menu-right">
+          <ul
+            class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5 text-overflow: ellipsis"
+          >
+            <li>About Us</li>
+            <li>Our Menu</li>
+            <li>Locations</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      </div>
+
+      <h1 class="text-white font-oswald uppercase text-6xl">Our Menu</h1>
+    </div>
+  </div>
+</template>
