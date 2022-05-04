8cd2d515ba82d8c0f39cec6db29ead5195340c19
WiseLee <wiseleelee@gmail.com>
2022-05-04 22:51:31 +0800

    Day8 Done

---
+++ components/MenuItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <div>
+      <img src="crispy-chicken.jpg" alt="" class="border border-gray p-5" />
+    </div>
+    <h4>Crispy Chicken</h4>
+    <p class="font-light text-gray-500 my-2">
+      Chicken breast, chili sauce, tomatoes, pickles, coleslaw
+    </p>
+    <p class="mt-5 mb-8">
+      <span
+        class="rounded rounded-lg bg-amber-800 text-yellow-400 font-oswald font-bold text-xl px-5 py-2"
+        >RM11.99</span
+      >
+    </p>
+    <button class="button font-oswald uppercase border py-2 px-3">Add to cart</button>
+  </div>
+</template>
--- pages/menu.vue
+++ pages/menu.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="bg-[url('/menu-page.jpg)]">
+    <div class="bg-[url('/menu-page.jpg')]">
       <div id="menu" class="grid grid-cols-2 py-5">
         <div id="menu-left">
           <img src="logo.png" class="w-[150px]" alt="" />
@@ -18,7 +18,14 @@
         </div>
       </div>
 
-      <h1 class="text-white font-oswald uppercase text-6xl">Our Menu</h1>
+      <h1 class="text-white font-oswald uppercase text-6xl text-center pt-24 pb-28">
+        Our Menu
+      </h1>
+    </div>
+    <div id="items" class="grid grid-cols-3">
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
     </div>
   </div>
 </template>
