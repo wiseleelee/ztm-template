3f9b11ae66d692e94da1d0a01653307190d629e7
WiseLee <wiseleelee@gmail.com>
2022-05-06 11:47:03 +0800

    day11

---
+++ pages/cart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="bg-[url('/menu-page.jpg')]">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+
+        <div id="menu-right">
+          <ul
+            class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5 text-overflow: ellipsis"
+          >
+            <li>About Us</li>
+            <NuxtLink to="/menu">Our Menu</NuxtLink>
+            <li>Contacts</li>
+            <NuxtLink to="/cart">Cart</NuxtLink>
+          </ul>
+        </div>
+      </div>
+
+      <h1 class="text-white font-oswald uppercase text-6xl text-center pt-24 pb-28">
+        Shopping Cart
+      </h1>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total</th>
+          <th>Remove</th>
+        </tr>
+      </thead>
+    </table>
+    <tbody>
+      <tr v-for="order in $store.state.orders" :key="order.name">
+        <td>{{ order.name }}</td>
+        <td>{{ order.price }}</td>
+        <td>{{ order.quantity }}</td>
+        <td>{{ order.price * order.quantity }}</td>
+        <td><button>✖️</button></td>
+      </tr>
+    </tbody>
+  </div>
+</template>
